7d0e7c1efa4133817c938ad9c76cd69870bd19cc
AmazonPart02 <thilinadhanushka0925@gmail.com>
2025-05-06 13:15:03 +0530

    17

--- tests-jasmine/moneyTest.js
+++ tests-jasmine/moneyTest.js
@@ -1,1 +1,7 @@
 import {formatCurrency} from '../scripts/utils/money.js';
+
+describe('test suite: formatCurrency', () => {
+  it('converts cents into dollars', () => {
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+});
