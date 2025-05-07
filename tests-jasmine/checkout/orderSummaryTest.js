3aba560337d770e8d3e55c5634a14b1156529be3
AmazonPart02 <thilinadhanushka0925@gmail.com>
2025-05-07 13:25:30 +0530

    78

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -5,5 +5,14 @@ describe('test suite: renderOrderSummary', () => {
       document.querySelector('.js-test-container').innerHTML = `
          <div class="js-order-summary"></div>
       `;
+      
+          spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([{
+              productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
+              quantity: 1,
+              deliveryOptionId: '1'
+            }]);
+          });
+          loadFromStorage();
   });
 });
