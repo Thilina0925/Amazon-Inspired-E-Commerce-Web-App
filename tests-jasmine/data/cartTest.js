0c17cdc44c13b605c05fa671476fdba186f337a2
AmazonPart02 <thilinadhanushka0925@gmail.com>
2025-05-07 11:30:56 +0530

    29

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,4 +1,4 @@
-import {addToCart} from '../../data/cart.js';
+import {addToCart, cart} from '../../data/cart.js';
 
 describe('test suite: addToCart', () => {
   it('adds an existing product to the cart', () => {
