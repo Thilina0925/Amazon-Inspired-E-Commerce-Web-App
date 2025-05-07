aa7929124654fe3d2e54faa702bd8360020437d0
AmazonPart02 <thilinadhanushka0925@gmail.com>
2025-05-07 12:25:09 +0530

    57

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -14,6 +14,7 @@ describe('test suite: addToCart', () => {
        spyOn(localStorage, 'getItem').and.callFake(() => {
          return JSON.stringify([{
           productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
+          quantity: 1,
          }]);
        });
        loadFromStorage();
