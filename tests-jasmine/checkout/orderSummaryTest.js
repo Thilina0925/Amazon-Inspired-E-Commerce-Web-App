4edeea4d347e5703c9443beb1723bb6c999d2fbf
AmazonPart02 <thilinadhanushka0925@gmail.com>
2025-05-07 22:22:24 +0530

    135

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -28,12 +28,6 @@ describe('test suite: renderOrderSummary', () => {
      renderOrderSummary();
   });
   it('displays the cart', () => {
-      document.querySelector('.js-test-container').innerHTML = `
-         <div class="js-order-summary"></div>
-      `;
-      
-          const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-          const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
           spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([{
               productId: productId1,
