34d1196e9313f87a7455d167e7bbd76b38698dbc
AmazonPart02 <thilinadhanushka0925@gmail.com>
2025-05-07 13:18:24 +0530

    74

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -2,6 +2,6 @@ import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
 
 describe('test suite: renderOrderSummary', () => {
   it('displays the cart', () => {
-      
+      document.querySelector('.js-test-container');
   });
 });
