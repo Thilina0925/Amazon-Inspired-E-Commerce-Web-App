13fe3366d79e827c3791d4533099d64164ad48a8
AmazonPart02 <thilinadhanushka0925@gmail.com>
2025-05-07 22:29:21 +0530

    Create automated tests for orderSummary

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -4,7 +4,7 @@ import {loadFromStorage, cart} from '../../data/cart.js';
 describe('test suite: renderOrderSummary', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
-  
+
   beforeEach(() => {
     spyOn(localStorage, 'setItem');
 
@@ -60,3 +60,7 @@ describe('test suite: renderOrderSummary', () => {
      document.querySelector('.js-test-container').innerHTML = '';
   });
 });
+
+
+
+
