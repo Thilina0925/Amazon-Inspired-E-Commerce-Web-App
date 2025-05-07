5326c025d86ca0370b9e9def0f4cf6918f1792ed
AmazonPart02 <thilinadhanushka0925@gmail.com>
2025-05-07 18:06:39 +0530

    93

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -25,5 +25,6 @@ describe('test suite: renderOrderSummary', () => {
           expect(
             document.querySelectorAll('.js-cart-item-container').length
           ).toEqual(2);
+          document.querySelector(``);
   });
 });
