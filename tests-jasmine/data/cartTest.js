e0f89bbeba49fb3298d35daa25aef074b4368f2a
AmazonPart02 <thilinadhanushka0925@gmail.com>
2025-05-07 12:34:45 +0530

    59

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -3,7 +3,11 @@ import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
 describe('test suite: addToCart', () => {
   it('adds an existing product to the cart', () => {
     spyOn(localStorage, 'getItem').and.callFake(() => {
-      return JSON.stringify([]);
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
     });
     loadFromStorage();
   });
@@ -12,11 +16,7 @@ describe('test suite: addToCart', () => {
        spyOn(localStorage, 'setItem');
 
        spyOn(localStorage, 'getItem').and.callFake(() => {
-         return JSON.stringify([{
-          productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
-          quantity: 1,
-          deliveryOptionId: '1'
-         }]);
+         return JSON.stringify([]);
        });
        loadFromStorage();
 
