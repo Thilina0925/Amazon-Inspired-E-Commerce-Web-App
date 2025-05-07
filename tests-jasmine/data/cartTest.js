e12813af1e6198679b20aab08e7096f8775ae8ff
AmazonPart02 <thilinadhanushka0925@gmail.com>
2025-05-07 12:26:00 +0530

    58

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -15,6 +15,7 @@ describe('test suite: addToCart', () => {
          return JSON.stringify([{
           productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
           quantity: 1,
+          deliveryOptionId: '1'
          }]);
        });
        loadFromStorage();
