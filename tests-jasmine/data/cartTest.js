2eb772692d4afe70951109c8936b529f3fe99608
AmazonPart02 <thilinadhanushka0925@gmail.com>
2025-05-07 11:58:12 +0530

    42

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -10,6 +10,7 @@ describe('test suite: addToCart', () => {
          return JSON.stringify([]);
        });
        console.log(localStorage.getItem('cart'));
+       loadFromStorage();
 
        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
        expect(cart.length).toEqual(1);
