d851ec3c14a69607ae332b39859a474fe6f92d60
AmazonPart02 <thilinadhanushka0925@gmail.com>
2025-05-08 15:08:17 +0530

    224

--- data/products.js
+++ data/products.js
@@ -10,6 +10,11 @@ export function getProduct(productId) {
   return matchingProduct;
 }
 
+
+class Product {
+  
+}
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
