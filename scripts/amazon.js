582cbe83f1a41b73d873a5828ad053bb889d8b96
AmazonPart02 <thilinadhanushka0925@gmail.com>
2025-05-08 16:02:42 +0530

    253

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-price">
-        $${formatCurrency(product.priceCents)}
+        ${product.getPrice()}
       </div>
 
       <div class="product-quantity-container">
